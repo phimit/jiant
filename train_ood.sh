caac9b28f6c0503e7727c5480a2ccd8d3c74ceae
phil_muller <philippe.muller@irit.fr>
2023-05-11 18:47:11 +0200

    fix train_ood

--- train_ood.sh
+++ train_ood.sh
@@ -7,9 +7,9 @@ export SPLIT=("disrpt23_tha_pdtb_tdtb_split" "disrpt23_deu_rst_pcc_split" "disrp
 export CONLLU=("disrpt23_tha_pdtb_tdtb_conllu" "disrpt23_deu_rst_pcc_conllu" "disrpt23_eng_rst_gum_conllu" "disrpt23_eng_rst_rstdt_conllu" "disrpt23_eng_sdrt_stac_conllu" "disrpt23_eus_rst_ert_conllu" "disrpt23_fas_rst_prstc_conllu" "disrpt23_fra_sdrt_annodis_conllu" "disrpt23_ita_pdtb_luna_conllu" "disrpt23_nld_rst_nldt_conllu" "disrpt23_por_rst_cstn_conllu" "disrpt23_rus_rst_rrt_conllu" "disrpt23_spa_rst_rststb_conllu" "disrpt23_spa_rst_sctb_conllu" "disrpt23_zho_dep_scidtb_conllu" "disrpt23_zho_pdtb_cdtb_conllu" "disrpt23_zho_rst_gcdt_conllu" "disrpt23_zho_rst_sctb_conllu")
 
 # these are done separately to have a specific transfer configuration
-export OOD_CONLLU=("disrpt23_tur_pdtb_tdb_conllu disrpt23_tur_pdtb_tedm_conllu" "disrpt23_eng_dep_scidtb_conllu disrpt23_eng_dep_covdtb_conllu" "disrpt23_eng_pdtb_pdtb_conllu disrpt23_eng_pdtb_tedm_conllu" "disrpt23_por_pdtb_crpc_conllu disrpt23_por_pdtb_tedm_conllu")
+#export OOD_CONLLU=("disrpt23_tur_pdtb_tdb_conllu disrpt23_tur_pdtb_tedm_conllu" "disrpt23_eng_dep_scidtb_conllu disrpt23_eng_dep_covdtb_conllu" "disrpt23_eng_pdtb_pdtb_conllu disrpt23_eng_pdtb_tedm_conllu" "disrpt23_por_pdtb_crpc_conllu disrpt23_por_pdtb_tedm_conllu")
 
-export OOD_SPLIT=("disrpt23_tur_pdtb_tdb_split disrpt23_tur_pdtb_tedm_split" "disrpt23_eng_dep_scidtb_split disrpt23_eng_dep_covdtb_split" "disrpt23_eng_pdtb_pdtb_split disrpt23_eng_pdtb_tedm_split" "disrpt23_por_pdtb_crpc_split disrpt23_por_pdtb_tedm_split")
+#export OOD_SPLIT=("disrpt23_tur_pdtb_tdb_split disrpt23_tur_pdtb_tedm_split" "disrpt23_eng_dep_scidtb_split disrpt23_eng_dep_covdtb_split" "disrpt23_eng_pdtb_pdtb_split disrpt23_eng_pdtb_tedm_split" "disrpt23_por_pdtb_crpc_split disrpt23_por_pdtb_tedm_split")
 
 export MODEL=xlm-roberta-large
 #export MODEL=bert-base-multilingual-uncased
@@ -27,12 +27,13 @@ export MODEL=xlm-roberta-large
 
 export EPOCHS=0.01
 
-for dataset in ${OOD_CONLLU[@]}; 
+for dataset in "disrpt23_tur_pdtb_tdb_conllu disrpt23_tur_pdtb_tedm_conllu" "disrpt23_eng_dep_scidtb_conllu disrpt23_eng_dep_covdtb_conllu" "disrpt23_eng_pdtb_pdtb_conllu disrpt23_eng_pd\
+tb_tedm_conllu" "disrpt23_por_pdtb_crpc_conllu disrpt23_por_pdtb_tedm_conllu"; 
 do
 echo "training on ..." $dataset
 set -- $dataset
-set -x
 echo "=== and doing OOD task" $2 
+set -x
 python simple_mtl_run.py "$1" --model-name $MODEL --epochs $EPOCHS --eval-every-step 500 \
 	--batch-size $BATCH_SIZE --gradient-accumulation-steps 4 \
 	--no_improvements_for_n_evals 10 --sampling-strategy ProportionalMultiTaskSampler --max-seq-length 180\
